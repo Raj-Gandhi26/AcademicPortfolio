7e1013acb08cc764a3232e11cde393c348e81f26
unknown <gandhiraj2605@gmail.com>
2025-11-14 17:46:38 +0530

    fix: add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <-- ADD THIS LINE RIGHT HERE
   server: {
     host: "::",
     port: 8080,
